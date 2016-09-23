f6498cb24a1f7fdd3f492945310f1f0ca05e8f75
Harminder Virk <virk.officials@gmail.com>
2016-09-24 00:30:58 +0530

    refactor(lint): make standardjs happy

--- src/Commands/check.js
+++ src/Commands/check.js
@@ -20,21 +20,21 @@ module.exports = function (argv) {
   const nodejsVersion = process.version.replace('v', '')
   const npmVersion = execSync('npm -v', {input: 'string', 'encoding': 'utf-8'})
 
-  console.log('-----------AdonisJS requirements check--------------')
-  console.log(`${colors.green('AdonisJS needs Node.JS '+requiredNodeVersion+' and npm '+requiredNpmVersion+'\n')}`)
+  console.log('-----------AdonisJs requirements check--------------')
+  console.log(`${colors.green('AdonisJs needs Node.JS ' + requiredNodeVersion + ' and npm ' + requiredNpmVersion + '\n')}`)
   console.log(`${colors.green('verifying node.js and npm current installed versions...')}`)
 
   console.log('\nYour current Node.JS version: ' + colors.yellow(nodejsVersion))
   console.log('Your current npm version: ' + colors.yellow(npmVersion))
 
   if (!semver.satisfies(nodejsVersion, requiredNodeVersion)) {
-    console.log(`${colors.red('\nYour current Node.js version does not met AdonisJS requirements.')}`)
+    console.log(`${colors.red('\nYour current Node.js version does not met AdonisJs requirements.')}`)
     console.log(`${colors.red('Please update your Node.JS version before continue.')}`)
     return
   }
 
   if (!semver.satisfies(npmVersion, requiredNpmVersion)) {
-    console.log(`${colors.red('\nYour Current npm version does not met AdonisJS requirements.')}`)
+    console.log(`${colors.red('\nYour Current npm version does not met AdonisJs requirements.')}`)
     console.log(`${colors.red('Please update your npm version with this command: ')}`)
     console.log(`${colors.yellow('npm install -g npm')}`)
     return
