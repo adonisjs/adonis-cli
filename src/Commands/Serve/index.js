34ee5024032b37e96711ce387654c9f229348ea8
Harminder Virk <virk.officials@gmail.com>
2017-07-25 19:47:46 +0100

    feat(commands): add serve command

---
+++ src/Commands/Serve/index.js
@@ -0,0 +1,87 @@
+'use strict'
+
+/*
+ * adonis-cli
+ *
+ * (c) Harminder Virk <virk@adonisjs.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+*/
+
+const forever = require('forever-monitor')
+const path = require('path')
+const { Command } = require('../../../lib/ace')
+
+/**
+ * Serve the application using forever
+ *
+ * @class Serve
+ * @constructor
+ */
+class Serve extends Command {
+  /**
+   * The command signature used by ace
+   *
+   * @method signature
+   *
+   * @return {String}
+   */
+  static get signature () {
+    return 'serve {--dev : Start development server}'
+  }
+
+  /**
+   * The command description used by ace
+   *
+   * @method description
+   *
+   * @return {String}
+   */
+  static get description () {
+    return 'Start Http server'
+  }
+
+  /**
+   * Method executed by ace to start the HTTP server
+   *
+   * @method handle
+   *
+   * @param  {Object} args
+   * @param  {Boolean} options.dev
+   *
+   * @return {void}
+   */
+  async handle (args, { dev }) {
+    const acePath = path.join(process.cwd(), 'ace')
+    const appFile = path.join(process.cwd(), 'server.js')
+    const exists = await this.pathExists(acePath)
+
+    if (!exists) {
+      this.error('Make sure you are inside an adonisjs app to start the server')
+      return
+    }
+
+    const child = new (forever.Monitor)(appFile, {
+      max: 1,
+      silent: false,
+      watch: dev,
+      watchDirectory: process.cwd()
+    })
+
+    console.log('')
+    if (dev) {
+      console.log(this.chalk.bgBlueBright(' Started server in dev mode '))
+    } else {
+      console.log(this.chalk.bgBlueBright(' Started server '))
+    }
+    console.log('')
+
+    child.on('watch:restart', (info) => {
+      console.log(`${this.chalk.magenta(info.file)}  ${info.stat.replace(process.cwd(), '')}`)
+    })
+    child.start()
+  }
+}
+
+module.exports = Serve
--- src/Commands/index.js
+++ src/Commands/index.js
@@ -16,5 +16,5 @@
  */
 module.exports = [
   require('./New'),
-  require('./Install')
+  require('./Serve')
 ]
