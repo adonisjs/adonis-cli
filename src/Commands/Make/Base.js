268241e4d338b020e8112366ac02ec57e3dfacd3
Chimezie Enyinnaya <meziemichael@gmail.com>
2018-01-25 14:52:02 +0100

    inline basename

--- src/Commands/Make/Base.js
+++ src/Commands/Make/Base.js
@@ -68,8 +68,7 @@ class MakeBase extends Command {
     const templateFile = path.join(__dirname, '../../Generators/templates', `${templateFor}.mustache`)
 
     const filePath = generators[templateFor].getFilePath(name, options)
-    const baseName = path.basename(name)
-    const data = generators[templateFor].getData(baseName, flags)
+    const data = generators[templateFor].getData(path.basename(name), flags)
 
     const templateContents = await this.readFile(templateFile, 'utf-8')
     await this.generateFile(filePath, templateContents, data)
